5a97ae274dfd677377f593f062893cffa10cad5f
henry-hiles <henry@henryhiles+.com>
2021-12-17 09:38:03 -0500

    Some UI changes, as well as adding a page for my flappybird project.

---
+++ Pages/Projects/FlappyBird.cshtml.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using DevTrends.Mailer;
+using HenryHiles.Pages._EmailTemplates;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HenryHiles.Helpers;
+using DevTrends.AwsClients;
+
+namespace HenryHiles.Pages;
+public class FlappyBirdModel : PageModel { }
--- TagHelpers/DividerTagHelper.cs
+++ TagHelpers/DividerTagHelper.cs
@@ -6,10 +6,25 @@ public class DividerTagHelper : TagHelper
     [HtmlAttributeName("dark")]
     public bool IsDark { get; set; } = false;
 
+    [HtmlAttributeName("round")]
+    public bool IsRound { get; set; } = false;
+
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
-        output.TagName = "i";
-        output.Attributes.SetAttribute("class", "divider-icon" + (IsDark ? " divider-icon-dark" : ""));
+        if (IsRound)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "round-divider");
+            output.Content.AppendHtml(@"<svg class=""round-divider-svg"" xmlns=""http://www.w3.org/2000/svg"" viewBox=""0 0 144.54 17.34"" preserveAspectRatio=""none""
+    fill=""currentColor"">
+    <path d=""M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0""></path>
+</svg>");
+        }
+        else
+        {
+            output.TagName = "i";
+            output.Attributes.SetAttribute("class", "divider-icon");
+        }
         output.TagMode = TagMode.StartTagAndEndTag;
     }
 }
